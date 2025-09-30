0249a115a034f248bfc7d194540c09d0639c21c3
Griffin Johnson <griffin.johnson@upstart.com>
2025-09-30 14:26:04 -0700

    update dots

---
+++ nvim/.config/nvim/lua/plugins/icon-picker.lua
@@ -0,0 +1,13 @@
+return {
+    "ziontee113/icon-picker.nvim",
+    config = function()
+        require("icon-picker").setup({ disable_legacy_commands = true })
+
+
+        vim.keymap.set("n", "<Leader>sp", "<cmd>IconPickerNormal<cr>", { noremap = true, silent = true, desc = 'Pick Icon' })
+        vim.keymap.set("n", "<Leader>sy", "<cmd>IconPickerYank<cr>", { noremap = true, silent = true, desc = 'Yank Icon' }) --> Yank the selected icon into register
+        vim.keymap.set("i", "<C-i>", "<cmd>IconPickerInsert<cr>", { noremap = true, silent = true, desc = 'Insert Icon' })
+   end
+}
+
+
